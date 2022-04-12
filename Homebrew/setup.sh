415a5bc97a811c0d736dbd02aa761479960883dc
5ouma <101255979+5ouma@users.noreply.github.com>
2022-04-12 18:12:11 +0900

    [update] Setup script and Brewfile
    Fix commands were comment out, add App Store login, Xcode install,install MesloLGS NF files and edit VSCode-Insiders.

--- Homebrew/setup.sh
+++ Homebrew/setup.sh
@@ -11,30 +11,45 @@ waitEnter() {
 cd ~/.terminal-files/Homebrew
 
 waitInput "Are you sure to start setup?"
-# xcode-select --install
+xcode-select --install
 
-sleep 3
+    sleep 3
 
+echo -e "Is commadnd correct?\n/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+    sleep 3
 open https://brew.sh/index_ja
-waitInput "Is commadnd correct?\n/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-waitEnter
-# brew doctor
-waitEnter
-# brew install mas
+    waitEnter
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    waitEnter
+brew doctor
+
+echo "Please download MesloLGS NF font"
+    sleep 3
+open https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+open https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+open https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+open https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+    waitEnter
 
 waitInput "If enter \"y\", start installing Homebrew packages and apps."
-# brew bundle
+echo "Please sigin in to App Store"
+    sleep 3
+open -a "App Store"
+    waitEnter
+brew bundle
 
-waitEnter
+    waitEnter
 
-# cp ~/.terminal-files/zsh/.zshenv ~
-# cp -r ~/.terminal-files/zsh/ ~
+cp ~/.terminal-files/zsh/.zshenv ~
+cp -r ~/.terminal-files/zsh/ ~
 
-# cp ~/.terminal-files/Hyper/.hyper.js ~
-# cp ~/.terminal-files/Hyper/.hyper_plugins ~
+cp ~/.terminal-files/Hyper/.hyper.js ~
+cp ~/.terminal-files/Hyper/.hyper_plugins ~
 
-# touch ~/.hushlogin
+touch ~/.hushlogin
 
-# cp ~/.terminal-files/Git/.gitconfig ~/.terminal-files/Git/.gitignore_global ~
-# cp -r ~/.terminal-files/Git/.ssh ~
+cp ~/.terminal-files/Git/.gitconfig ~/.terminal-files/Git/.gitignore_global ~
+cp -r ~/.terminal-files/Git/.ssh ~
+
+waitInput "Do you want to install Xcode?"
+mas install 497799835
