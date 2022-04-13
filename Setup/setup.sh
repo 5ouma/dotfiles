571421a8a1bc6f38c3e491fcb40fd29c8e87e1ef
5ouma <101255979+5ouma@users.noreply.github.com>
2022-04-13 20:39:07 +0900

    [update] setup.sh
    Add commands to set Launchpad size and add Space on Dock.

---
+++ Setup/setup.sh
@@ -0,0 +1,53 @@
+waitInput() {
+    echo -e -n "$1 (y/n): "
+    read -q && echo "" || {echo "" && exec $SHELL}
+}
+
+waitEnter() {
+    echo "Plese press enter."
+    read
+}
+
+cd ~/.terminal-files/Setup
+
+waitInput "Are you sure to start setup?"
+xcode-select --install
+
+    sleep 3
+
+echo -e "Is commadnd correct?\n/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+    sleep 3
+open https://brew.sh/index_ja
+    waitEnter
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    waitEnter
+brew doctor
+
+
+    waitInput "Copy terminal files."
+
+cp ~/.terminal-files/zsh/.zshenv ~
+cp -r ~/.terminal-files/zsh/ ~
+cp ~/.terminal-files/Hyper/.hyper.js ~
+cp ~/.terminal-files/Hyper/.hyper_plugins ~
+touch ~/.hushlogin
+cp ~/.terminal-files/Git/.gitconfig ~/.terminal-files/Git/.gitignore_global ~
+cp -r ~/.terminal-files/Git/.ssh ~
+cp ~/.terminal-files/Setup/Fonts/* ~/Library/Fonts
+
+    waitInput "Run changing Launchpad size and add space on Dock"
+defaults write com.apple.dock springboard-columns -int 9;defaults write com.apple.dock springboard-rows -int 7;defaults write com.apple.dock ResetLaunchPad -bool TRUE
+for ((i=0; i<6; i++)); do
+defaults write com.apple.dock persistent-apps -array-add '{tile-type="spacer-tile";}'
+done
+
+
+waitInput "If enter \"y\", start installing Homebrew packages and apps."
+echo "Please sigin in to App Store"
+    sleep 3
+open -a "App Store"
+    waitEnter
+brew bundle
+
+waitInput "Do you want to install Xcode?"
+mas install 497799835
