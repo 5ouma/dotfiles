331e59de410cc1f16edb9379c83a260fbe5ab8c9
5ouma <101255979+5ouma@users.noreply.github.com>
2022-04-25 20:51:14 +0900

    [update] Remove command to remove
    Make it possible to replace git files without removing.

--- sync.sh
+++ sync.sh
@@ -5,7 +5,6 @@ cp -r ~/.zsh ~/.shell-files/zsh
 
 
 # git
-rm -f ~/.shell-files/Git/.*
-cp ~/.gitconfig ~/.gitignore_global ~/.shell-files/Git
+cp -f ~/.gitconfig ~/.gitignore_global ~/.shell-files/Git
 echo "Do you use 1Password? (y/n): "
 read -q && (rm -f ~/.shell-files/Git/.ssh/1password/config && cp ~/.ssh/config ~/.shell-files/Git/.ssh/1password) || (rm -f ~/.shell-files/Git/.ssh/original/config && cp ~/.ssh/config ~/.shell-files/Git/.ssh/original)
