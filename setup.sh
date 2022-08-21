4325d4a7a285664707dc664ac941e7b17f1b2736
5ouma <101255979+5ouma@users.noreply.github.com>
2022-08-21 18:36:07 +0900

    [add] Show description of this repository
    Show it when running with the option `-h`.

--- setup.sh
+++ setup.sh
@@ -17,12 +17,15 @@ typeset -A langs=("nodejs" "Node.js")
 
 case "$1" in
 "-h" | "--help" )
+  echo -e "\033[32mdotfiles\033[m"
+  echo -e "My zsh, vim and other settings, plugins, etc and setup.\n"
+
   echo -e "\033[33mUSAGE:\033[m"
     echo -e "    source ~/.dotfiles/setup.sh\n"
   echo -e "\033[33mOPTIONS:\033[m"
     echo -e "    \033[32m-h\033[m, \033[32m--help\033[m    Print help information"
     echo -e "    \033[32m-y\033[m, \033[32m--yes\033[m     Run all configuration"
-    echo
+  echo
   exec $SHELL -l
   ;;
 "-y" | "--yes" )
