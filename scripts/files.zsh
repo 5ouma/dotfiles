685c989bdadcff96214377216af33f767df471e5
5ouma <101255979+5ouma@users.noreply.github.com>
2023-02-14 19:14:58 +0900

    [fix] Make `~/.cache`
    To export with lporg correctly.

--- scripts/files.zsh
+++ scripts/files.zsh
@@ -18,7 +18,7 @@ makeSymlink() {
 if $1 || "$doAll" || waitInput "Make symlinks or create terminal files." "$0"; then
   $1 || echoNumber "🔗 The following files and directories will be symlinked or created:"
   makeSymlink "$packageDir" "$HOME"
-  mkdir -p "$HOME/.vim/undo" "$HOME/.ssh/git"
+  mkdir -p "$HOME/.cache" "$HOME/.vim/undo" "$HOME/.ssh/git"
   touch "$HOME/.hushlogin"
   if ! "$notSetup"; then
     echoResult "Set these files!" "Setting files is failed."
