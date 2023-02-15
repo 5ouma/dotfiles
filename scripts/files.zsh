7dc6a27107311cfe0c42f976576970ae109efc74
5ouma <101255979+5ouma@users.noreply.github.com>
2023-02-15 16:51:04 +0900

    [change] Move and refactor symlinking
    Make it simpler and nicer.

--- scripts/files.zsh
+++ scripts/files.zsh
@@ -1,11 +1,26 @@
-if $1 || "$doAll" || waitInput "Make symlinks or create terminal files." "$0"; then
-  $1 || echoNumber "🔗 The following files and directories will be symlinked or created:"
-  makeSymlink "$packages" "$HOME"
+if $1 || "$doAll" || waitInput "Set files and directories." "$0"; then
   mkdir -p "$HOME/.cache" "$HOME/.vim/undo" "$HOME/.ssh/git"
   touch "$HOME/.hushlogin"
-  if ! "$notSetup"; then
-    echoResult "Set these files!" "Setting files is failed."
-  else
-    echoWarning "All files are already set."
-  fi
+
+  $1 || echoNumber "🔗" "Symlinking files..." && {
+    local notSetup=true
+    while read -r pack; do
+      local file >/dev/null 2>&1
+      file="$HOME/${pack#"$packages"/*/}"
+      if [[ -e "$file" && -n $(diff "$pack" "$file") ]]; then
+        mkdir -p "$backup" && mv "$file" "$backup/"
+      fi
+      if [[ ! -e "$file" ]]; then
+        mkdir -p "$(dirname "$file")" && ln -s "$pack" "$file"
+        print -- "- $(printc cyan false "~${file#~}")"
+        notSetup=false
+      fi
+    done < <(find "$packages" -type f ! -name ".DS_Store")
+
+    if ! "$notSetup"; then
+      echoResult "Symlinked these files!" "Symlinking files has failed."
+    else
+      echoWarning "All files are already symlinked."
+    fi
+  }
 fi
