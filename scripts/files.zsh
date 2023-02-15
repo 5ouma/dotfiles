b7abdb6722ed40a4a3b786376a52cf64304c64e3
5ouma <101255979+5ouma@users.noreply.github.com>
2023-02-15 13:16:07 +0900

    [change] Move `makeSymlink` function
    Move it to `setup` to manage all functions in it.

--- scripts/files.zsh
+++ scripts/files.zsh
@@ -1,20 +1,3 @@
-makeSymlink() {
-  while read -r pack; do
-    local homeFile homeDir >/dev/null 2>&1
-    homeFile="$2/$(echo "$pack" | sed -r "s/.*$(basename "$1")\/[^\/]*\///")"
-    homeDir=$(dirname "$homeFile")
-    if [[ -e "$homeFile" && -n $(diff "$pack" "$homeFile") ]]; then
-      mkdir -p "$(dirname "$1")/backup"
-      mv "$homeFile" "$(dirname "$1")/backup/"
-    fi
-    if [[ ! -e "$homeFile" || -n $(diff "$pack" "$homeFile") ]]; then
-      mkdir -p "$homeDir"
-      ln -sv "$pack" "$homeDir"
-      notSetup=false
-    fi
-  done < <(find "$1" -type f ! -name ".DS_Store")
-}
-
 if $1 || "$doAll" || waitInput "Make symlinks or create terminal files." "$0"; then
   $1 || echoNumber "🔗 The following files and directories will be symlinked or created:"
   makeSymlink "$packages" "$HOME"
