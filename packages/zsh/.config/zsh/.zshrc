da99eec40345a96077478f82ff1eccbbb213a914
5ouma <101255979+5ouma@users.noreply.github.com>
2023-09-29 21:14:59 +0900

    [revert] Adjust `|| :` to ignore an error
    Add the end of `.zshrc` with Fig block.

--- packages/zsh/.config/zsh/.zshrc
+++ packages/zsh/.config/zsh/.zshrc
@@ -7,4 +7,4 @@ for file in $files; do
 done
 
 # Fig post block. Keep at the bottom of this file.
-[[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.post.zsh"
+[[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.post.zsh" || :
--- packages/zsh/.config/zsh/extra/tools.zsh
+++ packages/zsh/.config/zsh/extra/tools.zsh
@@ -8,7 +8,7 @@ zsh-defer source "$XDG_CONFIG_HOME/op/plugins.sh"
 zsh-defer eval "$(atuin init zsh --disable-up-arrow)"
 
 # OrbStack
-zsh-defer source "$HOME/.orbstack/shell/init.zsh" 2>/dev/null || :
+zsh-defer source "$HOME/.orbstack/shell/init.zsh" 2>/dev/null
 
 # rtx
 eval "$(rtx activate -q zsh)"
